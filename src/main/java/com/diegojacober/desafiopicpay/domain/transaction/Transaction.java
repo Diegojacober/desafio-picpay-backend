f04bd024be84e31fd00cbf64fc396b9b20754b4c
Diego <diegoalencar.jacober@gmail.com>
2024-01-16 11:29:55 -0300

    fix: NoArgsConstructor

--- src/main/java/com/diegojacober/desafiopicpay/domain/transaction/Transaction.java
+++ src/main/java/com/diegojacober/desafiopicpay/domain/transaction/Transaction.java
@@ -13,10 +13,12 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "transactions")
 @Table(name = "transactions")
 @Data
+@NoArgsConstructor
 public class Transaction {
 
     @Id
--- src/main/java/com/diegojacober/desafiopicpay/domain/user/User.java
+++ src/main/java/com/diegojacober/desafiopicpay/domain/user/User.java
@@ -13,10 +13,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "users")
 @Table(name = "users")
 @Data
+@NoArgsConstructor
 public class User {
 
     @Id
