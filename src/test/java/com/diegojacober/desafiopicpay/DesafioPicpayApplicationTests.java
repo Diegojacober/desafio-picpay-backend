a94141d46391e1be763dfea2eda64353d4c74a68
Diego <diegoalencar.jacober@gmail.com>
2024-01-16 09:52:12 -0300

    :tada: Initial config

---
+++ src/test/java/com/diegojacober/desafiopicpay/DesafioPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegojacober.desafiopicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
